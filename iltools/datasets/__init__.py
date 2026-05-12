ff60e58a299286a3132c7f9d648fc6ac6fd8a4fd
Feiyang Wu <feiyangwu@gatech.edu>
2026-05-12 17:00:14 -0400

    Make dataset loaders lazy and optional

--- iltools/datasets/__init__.py
+++ iltools/datasets/__init__.py
@@ -1,1 +1,9 @@
 """Dataset package."""
+
+from .loaders import (  # noqa: F401
+    DatasetLoaderSpec,
+    get_dataset_loader_spec,
+    load_dataset_loader,
+    register_dataset_loader,
+    registered_dataset_loaders,
+)
